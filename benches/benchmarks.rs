edec8b7ed058c07bdda6460b82ccce75ce28a6f2
Esteban Ginez <175813+eginez@users.noreply.github.com>
2024-08-18 22:09:48 -0700

    fixes benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,7 +1,7 @@
 extern crate criterion;
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use isomorphic_graph_creation::unrank;
+use isomorphic_graph_creation::{unrank, unrank_parallel};
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use std::time::Duration;
@@ -17,13 +17,14 @@ fn _create_random_input(size: u32) -> Vec<u64> {
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("single_unrank");
     group.measurement_time(Duration::new(10, 0));
+    group.sample_size(10);
 
-    const GRAPH_SIZE: u32 = 50;
+    const GRAPH_SIZE: u32 = 22;
     const SUBSET_SIZE: u64 = 10;
     let input = _create_random_input(GRAPH_SIZE);
     group.bench_function("single_unrank", |b| b.iter(|| unrank(&input, SUBSET_SIZE)));
     group.bench_function("parallel_unrank", |b| {
-        b.iter(|| unrank(&input, SUBSET_SIZE))
+        b.iter(|| unrank_parallel(&input, SUBSET_SIZE))
     });
 }
 
