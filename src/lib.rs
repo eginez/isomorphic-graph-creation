f6f2f232ad069824125c21ebea2b45f1d6bcf794
Esteban Ginez <175813+eginez@users.noreply.github.com>
2024-08-18 20:21:27 -0700

    optimization for bc calc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+use std::hash::Hash;
 use std::iter::Product;
 
 use num::{self, PrimInt};
@@ -15,21 +17,25 @@ where
 }
 pub fn binomial_coefficient<T>(n: T, r: T) -> T
 where
-    T: Integer + PrimInt + Unsigned + Product,
+    T: Integer + PrimInt + Unsigned + for<'a> Product<&'a T> + Hash
 {
-    factorial(n) / (factorial(r) * factorial(n - r))
+    let first_list : HashSet<T> = num::range_inclusive(r + T::one(), n).collect();
+    let second_list : HashSet<T> = num::range_inclusive(T::one(), n - r).collect();
+    let numerator = first_list.difference(&second_list);
+    let denominator = second_list.difference(&first_list);
+    numerator.product::<T>() / denominator.product::<T>()
 }
 
 pub fn unrank_combination_single(
-    set: &Vec<usize>,
-    mut subset_size: usize,
-    mut rank: usize,
-) -> Vec<usize> {
-    let mut combination = vec![];
-    let mut i = 0;
-    let n = set.len();
+    set: &Vec<u64>,
+    mut subset_size: u64,
+    mut rank: u64,
+) -> Vec<u64> {
+    let mut combination:Vec<u64> = vec![];
+    let mut i: usize = 0;
+    let n = set.len() as u64;
     loop {
-        let c = binomial_coefficient(n - (i + 1), subset_size - 1);
+        let c = binomial_coefficient(n - (i as u64 + 1), subset_size - 1);
         if rank < c {
             combination.push(set[i]);
             subset_size -= 1;
@@ -46,16 +52,16 @@ pub fn unrank_combination_single(
     }
     combination
 }
-pub fn unrank(set: &Vec<usize>, subset_size: usize) -> Vec<Vec<usize>> {
-    let num_combinations = binomial_coefficient(set.len(), subset_size);
+pub fn unrank(set: &Vec<u64>, subset_size: u64) -> Vec<Vec<u64>> {
+    let num_combinations = binomial_coefficient(set.len() as u64, subset_size);
     (0..num_combinations)
         .into_iter()
         .map(|pos| unrank_combination_single(set, subset_size, pos))
         .collect()
 }
 
-pub fn unrank_parallel(set: &Vec<usize>, subset_size: usize) -> Vec<Vec<usize>> {
-    let num_combinations = binomial_coefficient(set.len(), subset_size);
+pub fn unrank_parallel(set: &Vec<u64>, subset_size: u64) -> Vec<Vec<u64>> {
+    let num_combinations = binomial_coefficient(set.len() as u64, subset_size);
     (0..num_combinations)
         .into_par_iter()
         .map(|pos| unrank_combination_single(set, subset_size, pos))
@@ -74,8 +80,7 @@ mod tests {
     #[case(1, 1)]
     #[case(2, 2)]
     #[case(3, 6)]
-    #[case(20, 3628800)]
-    fn test_factorial(#[case] number: usize, #[case] result: usize) {
+    fn test_factorial(#[case] number: u64, #[case] result: u64) {
         assert_eq!(factorial(number), result);
     }
 
@@ -84,8 +89,8 @@ mod tests {
     #[case(3, 1, 3)]
     #[case(3, 2, 3)]
     #[case(2, 0, 1)]
-    #[case(20, 5, 1)]
-    fn test_binomial_coefficient(#[case] n: usize, #[case] r: usize, #[case] result: usize) {
+    #[case(20, 5, 15504)]
+    fn test_binomial_coefficient(#[case] n: u64, #[case] r: u64, #[case] result: u64) {
         assert_eq!(binomial_coefficient(n, r), result);
     }
 
@@ -94,10 +99,10 @@ mod tests {
     #[case(vec![5,8,0], 2, 1, vec![5,0])]
     #[case(vec![5,8,0], 2, 2, vec![8, 0])]
     fn test_rank(
-        #[case] set: Vec<usize>,
-        #[case] subset_size: usize,
-        #[case] r: usize,
-        #[case] res: Vec<usize>,
+        #[case] set: Vec<u64>,
+        #[case] subset_size: u64,
+        #[case] r: u64,
+        #[case] res: Vec<u64>,
     ) {
         assert_eq!(unrank_combination_single(&set, subset_size, r), res);
     }
@@ -105,7 +110,7 @@ mod tests {
     #[test]
     fn test_rank_complete() {
         let input = vec![5, 8, 0, 1];
-        let all_results: Vec<Vec<usize>> = unrank(&input, 2);
+        let all_results: Vec<Vec<u64>> = unrank(&input, 2);
         assert_eq!(all_results[0], vec![5, 8]);
         assert_eq!(all_results[1], vec![5, 0]);
         assert_eq!(all_results[2], vec![5, 1]);
@@ -116,7 +121,7 @@ mod tests {
     #[test]
     fn test_rank_complete_parallel() {
         let input = vec![5, 8, 0, 1];
-        let all_results: Vec<Vec<usize>> = unrank_parallel(&input, 2);
+        let all_results: Vec<Vec<u64>> = unrank_parallel(&input, 2);
         assert_eq!(all_results[0], vec![5, 8]);
         assert_eq!(all_results[1], vec![5, 0]);
         assert_eq!(all_results[2], vec![5, 1]);
@@ -124,7 +129,7 @@ mod tests {
         assert_eq!(all_results[4], vec![8, 1]);
         assert_eq!(all_results[5], vec![0, 1]);
     }
-    fn _create_random_input(size: u32) -> Vec<usize> {
+    fn _create_random_input(size: u32) -> Vec<u64> {
         let mut rng = StdRng::seed_from_u64(32);
         (0..size)
             .into_iter()
@@ -135,7 +140,7 @@ mod tests {
     #[test]
     fn test_rank_big() {
         let input = _create_random_input(40);
-        let all_results: Vec<Vec<usize>> = unrank_parallel(&input, 2);
+        let all_results: Vec<Vec<u64>> = unrank_parallel(&input, 2);
         assert!(all_results.len() > 0)
     }
 }
